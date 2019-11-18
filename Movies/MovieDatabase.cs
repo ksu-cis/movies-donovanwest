d3710366d6fe025d79c748b1ab9dc4857254db3c
donovanwest <donovanwest@F1092-10-2098>
2019-11-18 15:04:41 -0600

    :)

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,38 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string search, List<string> rating)
+        {
+            if(search == null && rating.Count == 0)
+            {
+                return All;
+            }
+            List<Movie> result = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                if (search != null && rating.Count > 0)
+                {
+                    if (movie.Title != null && movie.Title.Contains(search, StringComparison.InvariantCultureIgnoreCase) && rating.Contains(movie.MPAA_Rating))
+                    {
+                        result.Add(movie);                       
+                    }
+                }
+                else if (search != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(search, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        result.Add(movie);
+                    }
+                }
+                else if(rating.Count > 0){
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        result.Add(movie);
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
